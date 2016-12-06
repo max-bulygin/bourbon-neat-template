9007387eaedfe17dbf1ee31f5e555ffb438b49b8
Max Bulygin <maksym.bulygin@gmail.com>
2016-12-06 17:59:13 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var rigger      = require('gulp-rigger');
+
+gulp.task('serve', ['sass', 'rigger'], function() {
+
+    browserSync.init({
+        server: "./build"
+    });
+
+    gulp.watch('./dev/**/*.html', ['rigger']);
+    gulp.watch('./dev/scss/**/*.s*ss', ['sass']);
+    gulp.watch('./build/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+    return gulp.src('dev/scss/*.s*ss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./build/css/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('rigger', function () {
+    gulp.src('./dev/*.html')
+        .pipe(rigger())
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('default', ['serve']);

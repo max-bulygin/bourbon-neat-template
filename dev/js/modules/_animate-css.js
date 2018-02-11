5301a7a4a2a9090f71a8b9f06bf75b545e1a5a2f
mbulygin <maksym.bulygin@gmail.com>
2018-02-12 00:48:09 +0300

    extend jQuery with animation function

---
+++ dev/js/modules/_animate-css.js
@@ -0,0 +1,29 @@
+// Usage: https://github.com/daneden/animate.css
+var $ = require('jquery');
+
+$.fn.extend({
+    animateCss: function(animationName, callback) {
+        var animationEnd = (function(el) {
+            var animations = {
+                animation: 'animationend',
+                OAnimation: 'oAnimationEnd',
+                MozAnimation: 'mozAnimationEnd',
+                WebkitAnimation: 'webkitAnimationEnd',
+            };
+
+            for (var t in animations) {
+                if (el.style[t] !== undefined) {
+                    return animations[t];
+                }
+            }
+        })(document.createElement('div'));
+
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+
+            if (typeof callback === 'function') callback();
+        });
+
+        return this;
+    }
+});
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,5 @@
 global.jQuery = global.$ = require('jquery');
+require('./modules/_animate-css');
 var svg4everybody = require('svg4everybody');
 var waypoints = require('modules/_waypoints');
 var smoothScroll = require('./modules/_smooth-scroll');

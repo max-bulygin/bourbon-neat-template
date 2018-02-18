cd54d00d679f5fce8e7f120cdb0cf7f1c609d2be
mbulygin <maksym.bulygin@gmail.com>
2018-02-18 17:03:50 +0300

    fixed typo in path

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,7 @@
 global.jQuery = global.$ = require('jquery');
 require('./modules/_animate-css');
 var svg4everybody = require('svg4everybody');
-var waypoints = require('modules/_waypoints');
+var waypoints = require('./modules/_waypoints');
 var smoothScroll = require('./modules/_smooth-scroll');
 
 $(document).ready(function () {
